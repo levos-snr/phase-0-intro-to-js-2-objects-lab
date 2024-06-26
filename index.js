7f94d83d337d446592b3db08c81aa5506dffc114
Lewis Odero <lewisodero27@gmail.com>
2024-06-26 16:07:20 +0300

    feat: add logic in index.js for the provided test cases

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Lewis ',
+  streetAddress : '123 Main Street'
+}
+
+// ✓ returns an employee with the original key value pairs and the new key value pair
+// ✓ it does not modify the original employee, but rather returns a clone with the new data
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+// ✓ updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key] = value;
+  return employee;
+}
+
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Sam'));
+
+// ✓ deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+// ✓ does not modify the original employee (it is non-destructive)
+function deleteFromEmployeeByKey(employee, key){
+  const newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+console.log(deleteFromEmployeeByKey(employee, 'name'),`Deleted`);
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee;
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee, 'name'),`Deleted 2`);
+
+
+
+
+
